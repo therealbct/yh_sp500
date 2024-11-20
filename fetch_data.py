c44f48903ae5959dda58c01f0836c2a01fae9df8
BCT <87674835+therealbct@users.noreply.github.com>
2024-11-19 19:49:57 -0500

    initial commit

---
+++ fetch_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import yfinance as yf
+
+def get_sp500_tickers():
+    sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+    return sp500['Symbol'].tolist()
+
+def get_additional_etfs():
+    additional_etfs = [
+        'SPY', 'XOP', 'XLE', 'USO', 'DBC', 'GLD', 'JETS', 'PEJ',
+        'VNQ', 'IYR', 'HYG', 'JNK', 'ANGL', 'DVY', 'VYM', 'SDIV', 'EMB', 'HYEM'
+    ]
+    return additional_etfs
+
+def fetch_and_save_data():
+    tickers = get_sp500_tickers() + get_additional_etfs()
+    data = yf.download(tickers, start="2015-01-01", end="2024-12-31", progress=False)['Adj Close']
+    data.to_csv("sp500_etf.csv")
+    
+if __name__ == "__main__":
+    fetch_and_save_data()
