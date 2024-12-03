ccde06061d55f9cf77afd739e62a46e544270693
BCT <87674835+therealbct@users.noreply.github.com>
2024-12-03 10:44:48 -0500

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import yfinance as yf
 from datetime import datetime
 import pytz
+import sys  # Required for redirecting print statements to stderr
 
 def get_sp500_tickers():
     sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
@@ -16,14 +17,12 @@ def get_additional_etfs():
 
 def fetch_and_save_data():
     tickers = get_sp500_tickers() + get_additional_etfs()
-    print("Downloading data..")
+    print("Downloading data..", file=sys.stderr)
     data = yf.download(tickers, start="2015-01-01", end=datetime.now(pytz.timezone("America/New_York")), progress=False)['Adj Close']
-    # data.to_parquet("sp500_etf.parquet")
-    # data.to_pickle("sp500_etf.pkl")
 
-    print("Saving to sp500_etf.parquet..")
+    print("Saving to sp500_etf.parquet..", file=sys.stderr)
     data.to_parquet("sp500_etf.parquet", engine='pyarrow')
-    print("Data saved to sp500_etf.parquet")
+    print("Data saved to sp500_etf.parquet", file=sys.stderr)
 
 if __name__ == "__main__":
     fetch_and_save_data()
